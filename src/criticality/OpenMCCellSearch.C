84311ab71ae280a64b2950fddfdc127a756f038a
lewisgross1296 <ligross@wisc.edu>
2025-09-03 22:24:20 -0500

    cell search

--- src/criticality/OpenMCCellSearch.C
+++ src/criticality/OpenMCCellSearch.C
@@ -1,21 +1,3 @@
-/********************************************************************/
-/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
-/*                             Cardinal                             */
-/*                                                                  */
-/*                  (c) 2021 UChicago Argonne, LLC                  */
-/*                        ALL RIGHTS RESERVED                       */
-/*                                                                  */
-/*                 Prepared by UChicago Argonne, LLC                */
-/*               Under Contract No. DE-AC02-06CH11357               */
-/*                With the U. S. Department of Energy               */
-/*                                                                  */
-/*             Prepared by Battelle Energy Alliance, LLC            */
-/*               Under Contract No. DE-AC07-05ID14517               */
-/*                With the U. S. Department of Energy               */
-/*                                                                  */
-/*                 See LICENSE for full restrictions                */
-/********************************************************************/
-
 #ifdef ENABLE_OPENMC_COUPLING
 
 #include "OpenMCCellSearch.h"
@@ -37,11 +19,24 @@ OpenMCCellSearch::OpenMCCellSearch(const InputParameters & parameters)
 {
   for (auto cell_id : _cell_id_list )
   {
-    // TODO: update for cells: int err = openmc_get_material_index(_cell_id_list, &_material_index);
-    catchOpenMCError(err, "get index for cell with ID " + std::to_string(cell_id));
-
-    // TODO: do we need to test that the given cell already has a rotation
+    // get cell idx for the current cell_id
+    int idx = 0;
+    int cell_err = openmc_get_cell_index(cell_id, &idx)
+    catchOpenMCError(cell_err, "get index for cell with ID " + std::to_string(cell_id));
+    // get the fill for this cell
+    int fill_type;
+    int32_t * materials = nullptr;
+    int n_materials = 0;
+    int fill_err = openmc_cell_get_fill(idx, &fill_type, &materials, &n_materials);
+    catchOpenMCError(fill_err, "get fill of cell " + printCell(cell_info));
+    if( fill_type == openmc::Fill:UNIVERSE)
+      _cell_index_list.push_back(idx) // add cell if it's in the cell_id_list and is filled with a universe
+    else {
+      mooseWarning("You have included " + std::tostring(cell_id) + " in the cell_id_list " +
+                   "however it is not filled with an openmc.Universe. You can only specify " +
+                   "rotations on cells filled with universes, therefore this cell will not" +
+                   "participate in the search.")
+    }
   }
 }
-
 #endif
